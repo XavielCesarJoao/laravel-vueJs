c8d16b886bfa065aa24c93367f86294ed23c47cb
XavielCesarJoao <134645093+XavielCesarJoao@users.noreply.github.com>
2025-02-10 13:07:23 +0100

    Ate aqui conseguimos implementar o fortfy do laravel para autenticação

---
+++ src/views/UserViews.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Usuarios ativos</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/auth/Dashboard.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+import axiosInstance from "@/lib/Axios";
+import {ref} from "vue";
+
+const user = ref({
+  name: "",
+  email: "",
+});
+
+const getUser = async () => {
+  try {
+    const response = await axiosInstance.get('/user');
+    user.value = response.data;
+  }catch (error){
+    console.log("PORRAZ");
+  }
+}
+const logout = async () => {
+  try{
+    const response = await axiosInstance.post('/logout');
+    console.log(response.data);
+  }catch (error){
+    console.error(error);
+  }
+}
+
+getUser();
+
+</script>
+
+<template>
+<div class="p-5">
+  <h1 class="text-3xl text-red-800">Dasboard</h1>
+  <p>{{user.name}}</p>
+  <br>
+  <p>{{user.email}}</p>
+  <div>
+    <button @click="logout" class="btn btn-primary">Logout</button>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
